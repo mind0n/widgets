c0b62d3325ddeaa348786e9487ade569c7d12795
mind0n <weerspecial@gmail.com>
2016-06-27 15:11:34 +0800

    Cleanup

--- assets/src/tests/entry.js
+++
@@ -1,6 +0,0 @@
-//require ("./common/reset.css");
-// require ("./common/toastr.css");
-// require ("./common/common.js");
-// require ("./common/jquery.js");
-require ("./tests/test.ts");
-require ("./tests/testr.ts");
