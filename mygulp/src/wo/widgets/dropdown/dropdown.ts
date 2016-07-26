71f0c88fbffba28c606cbec24f3023a59062c536
Mark Yuan <weerspecial@126.com>
2016-07-26 10:48:45 +0800

    Dropdown in progress

---
+++ mygulp/src/wo/widgets/dropdown/dropdown.ts
@@ -0,0 +1,44 @@
+/// <reference path="../../foundation/elements.ts" />
+/// <reference path="../../builder/uicreator.ts" />
+
+namespace wo{
+    Widgets.dropdown = function():any{
+        return  {
+            tag:"div",
+            class:"dropdown",
+            setval: function(val:any):void{
+                for(let i in val){
+                    let v = val[i];
+                    let t = this["$" + i];
+                    if (t){
+                        if (typeof (v) == 'object'){
+                            if (!v.mode || (v.mode == "prepend" && t.childNodes.length < 1)){
+                                v.mode = "append";
+                            }
+                            if (v.mode == "replace"){
+                                t.innerHTML = "";
+                                v.mode = "append";
+                            }
+                            if (v.mode == "prepend"){
+                                t.insertBefore(v.target, t.childNodes[0]);
+                            }else{
+                                t.appendChild(v.target);
+                            }                            
+                        }else{
+                            $(t).text(v);
+                        }
+                    }
+                }            
+            },
+            $:[
+                {tag:"div", class:"title noselect", $:[
+                    {tag:"div", class:"txt", alias:"title"},
+                    {tag:"div", class:"ctrls", $:[
+                        {tag:"div", class:"wbtn", onclick: function(event:any){wo.destroy(this.$border);}, $:"X"}
+                    ]}
+                ]},
+                {tag:"div", class:"body", alias:"body"}
+            ]
+        };
+    }
+}
