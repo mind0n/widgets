23ec478d8cd626bcb0021e4d729dd6fba11ddc0b
Mark Yuan <weerspecial@126.com>
2016-08-02 09:59:48 +0800

    Mocha & Chai

---
+++ utmocha/spec/tests.ts
@@ -0,0 +1,26 @@
+/// <reference path="../typings/index.d.ts" />
+
+import {sayHello} from "../src/component/greet.ts";
+import {showHello} from "../src/component/greet.ts";
+
+import {assert} from "chai";
+
+describe("Unit test suite", function(){
+    it("sayHello & showHello should exist", function(){
+        assert.isFunction(sayHello);
+        assert.isFunction(showHello);
+    });
+    // it("showHello should work", function(){
+    //     let id = "helloDiv";
+    //     let div = document.getElementById(id);
+    //     if (!div){
+    //         div = document.createElement('div');
+    //         div.id = id;
+    //         div.style.display = 'none';
+    //         document.body.appendChild(div);
+    //     }        
+    //     let content = "Unit Test";
+    //     showHello(id, content);
+    //     assert.equal(div.innerHTML, "Hello from Unit Test");
+    // });
+});
---
+++ utmocha/src/main.ts
@@ -0,0 +1,4 @@
+import { sayHello } from "./component/greet";
+import { showHello } from "./component/greet";
+
+showHello("greeting", "unit test source code");
