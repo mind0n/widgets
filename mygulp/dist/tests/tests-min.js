09249fd4d5053d7b045527c70823c04169c80b72
weerspecial@gmail.com <weerspecial@gmail.com>
2016-08-06 14:08:37 +0800

    Remove

--- mygulp/dist/tests/tests-min.js
+++
@@ -1,1 +0,0 @@
-describe("wo.js test suite",function(){it("wo should exist",function(){expect(wo).toBeTruthy(),expect(wo.use).toBeTruthy()}),it("UiCreator should exist",function(){var t=new wo.UiCreator;expect(t).toBeTruthy()})});
