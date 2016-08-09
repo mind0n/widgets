7d079df1953f6096e6215b4cb2988155d718acb9
Mark Yuan <weerspecial@126.com>
2016-08-09 10:03:46 +0800

    remove file

--- greet.ts
+++
@@ -1,8 +0,0 @@
-export function sayHello(name: string) {
-    return `Hello from ${name}`;
-}
-
-export function showHello(divName: string, name: string) {
-    let elt = document.getElementById(divName);
-    elt.innerText = sayHello(name);
-}
