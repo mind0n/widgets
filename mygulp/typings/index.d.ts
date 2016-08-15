f495e1ee99eff58ecd6a9fd92a2215c06c524d28
Mark Yuan <weerspecial@126.com>
2016-08-15 19:23:36 +0800

    Refine

--- mygulp/typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
