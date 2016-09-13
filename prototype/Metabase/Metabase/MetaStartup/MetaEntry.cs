a0f791510487a7a48d80500feeeee29c4d29bebf
weerspecial@gmail.com <weerspecial@gmail.com>
2016-09-13 23:08:02 +0800

    Metabase prototype

---
+++ prototype/Metabase/Metabase/MetaStartup/MetaEntry.cs
@@ -0,0 +1,26 @@
+﻿
+using Act.Core;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metabase
+{
+    public class MetaEntry
+    {
+		protected static dynamic cfg { get; set; }
+		public static void Start(dynamic settings)
+		{
+			log.Register(new ConsoleLogManager());
+			cfg = settings;
+			log.i($"Meta module loaded");
+		}
+		public static void Stop()
+		{
+			log.i($"Meta module unloaded");
+		}
+	}
+}
---
+++ prototype/Metabase/Metabase/MetaStartup/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Act.Core;
+using Act.Infrastructure;
+using Foundation;
+using Native;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act
+{
+    class Startup
+    {
+        static void Main(string[] args)
+		{
+			//var gh = new GlobalKeyboardHook();
+			//gh.HookedKeys.Add(Keys.LMenu);
+			//gh.HookedKeys.Add(Keys.OemSemicolon);
+			log.Register(new ConfigLogManager());
+			Cmd.Handle(args, ()=>
+			{
+				var dir = Environment.CurrentDirectory;
+				ServiceStartup ss = new ServiceStartup();
+				if (Cmd.Exists("c") || Debugger.IsAttached)
+				{
+					ss.Begin();
+					log.w("Press ENTER to stop server ...");
+					Console.ReadLine();
+					ss.End();
+					log.w("Press any key to exit ...");
+					Console.ReadKey();
+				}
+				else
+				{
+					ServiceBase[] svc = { ss };
+					ServiceBase.Run(svc);
+				}
+			});
+
+		}
+
+		private static void Output(string msg)
+        {
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "output.txt", msg);
+        }
+    }
+}
