ec457a6f4491e253b264e105c98bbe270f5bced0
weerspecial@gmail.com <weerspecial@gmail.com>
2016-09-17 23:31:50 +0800

    Nuget update

---
+++ prototype/Metabase/Metabase.Entry/sample.entry.cs
@@ -0,0 +1,25 @@
+﻿using Act.Core;
+using Foundation;
+using System;
+
+namespace Act.Entry
+{
+	/// <summary>
+	/// Please modify module.act.json (will be generated in non-library project such as console application project or windows application project)
+	///	from	"pattern": "Act.Infrastructure.dll",
+	/// to		"pattern": "Current assembly (.dll)",
+	/// </summary>
+	public class SampleEntry
+	{
+		
+		public static void Start(dynamic settings)
+		{
+			log.d($"SampleEntry starting - {AppDomain.CurrentDomain.FriendlyName} -> {settings.test}");
+		}
+
+		public static void Stop()
+		{
+			log.d($"SampleEntry stopping - {AppDomain.CurrentDomain.FriendlyName}");
+		}
+	}
+}
---
+++ prototype/Metabase/Metabase.Startup/Act.Startup.cs
@@ -0,0 +1,36 @@
+﻿using Act.Core;
+using Act.Infrastructure;
+using Foundation;
+using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace Act
+{
+    class Startup
+    {
+        static void Main(string[] args)
+		{
+			log.Register(new ConfigLogManager());
+			Cmd.Handle(args, ()=>
+			{
+				ServiceStartup ss = new ServiceStartup();
+				if (!Cmd.Exists("s") || Debugger.IsAttached)
+				{
+					ss.Begin();
+					log.w("Press ENTER to unload module(s) ...");
+					Console.ReadLine();
+					ss.End();
+					log.w("Press any key to exit ...");
+					Console.ReadKey();
+				}
+				else
+				{
+					ServiceBase[] svc = { ss };
+					ServiceBase.Run(svc);
+				}
+			});
+
+		}
+    }
+}
