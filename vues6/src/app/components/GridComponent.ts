6b86f4795adabce607a8e8b57c0654a1883b7a89
mark.yuan <mark.yuan@greendotcorp.com>
2017-05-03 15:02:38 +0800

    Grid rendering in progress

--- vues6/src/app/components/GridComponent.ts
+++ vues6/src/app/components/GridComponent.ts
@@ -1,17 +1,50 @@
 import * as Vue from 'vue'
 import Component from 'vue-class-component'
 import {Widget} from './widget'
+import {each} from '../../../../../kernel/web/common'
 
+@Component({
+    template: `
+        <div class="w-cell"><slot></slot></div>
+    `
+    , props:["meta", "dat", "field"]
+    , components:{
+        
+    }
+})
+class Cell extends Widget{
+    protected meta:any;
+    protected dat:any;
+    protected field:string;
+    columns(){
+        return this.meta?this.meta.columns:[];
+    }
+    mounted(){
+        let f = this.field;
+        let m = this.meta;
+        console.log(m);
+        if (this.dat && m && f){
+            let d = this.dat[f];
+            if (d){
+                console.log(d);
+                this.val(d);
+            }
+        }
+    }
+    protected val(v:string){
+        this.$el.innerHTML = v;
+    }
+}
 
 @Component({
     template: `
         <div class="w-head w-row">
-            <div v-for="item in columns()" v-if="!item.hidden" class="w-cell">{{item.caption}}</div>
+            <Cell v-for="item in columns()" v-if="!item.hidden" :key="$uid()">{{item.caption}}</Cell>
         </div>
     `
     , props:["meta"]
     , components:{
-        
+        Cell
     }
 })
 class HRow extends Widget{
@@ -23,21 +56,31 @@ class HRow extends Widget{
 
 @Component({
     template: `
-        <div class="w-row">
-            Row
-        </div>
+        <div class="w-row"><Cell v-for="item in columns()" v-if="!item.hidden" :meta="item" :field="item.field" :dat="cells()" :key="$uid()"></Cell></div>
     `
-    , props:["alive", "current"]
+    , props:["dat", "meta"]
     , components:{
-        
+        Cell
     }
 })
 class Row extends Widget{
+    protected dat:any;
+    protected meta:any;
+    columns(){
+        return this.meta?this.meta.columns:[];
+    }
+    cells(){
+        return this.dat||[];
+    }
+
 }
 @Component({
     template: `
         <div class="w-grid">
             <HRow :meta="getmeta()"></HRow>
+            <div class="w-body">
+                <Row v-for="row in getdata()" :dat="row" :meta="getmeta()" :key="$uid()" />
+            </div>
         </div>
     `
     , props:[]
@@ -49,6 +92,9 @@ export class GridComponent extends Widget{
     getmeta(){
         return this.dat.meta;
     }
+    getdata(){
+        return this.dat.value;
+    }
     protected dat:any;
     constructor(options?: Vue.ComponentOptions<Vue>){
         super(options);
@@ -56,5 +102,8 @@ export class GridComponent extends Widget{
     }
     prepare(meta){
         this.$set(this.dat, 'meta', meta);
+    }
+    bind(dat){
+        this.$set(this.dat, 'value', dat);
     }
 }
