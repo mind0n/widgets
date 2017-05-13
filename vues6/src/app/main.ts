2f6d35cd5ef23c83981b6e1fbe329bd600c80b18
mark.yuan <mark.yuan@greendotcorp.com>
2017-05-13 23:30:14 +0800

    Custom up-loader in progress

--- vues6/src/app/main.ts
+++ vues6/src/app/main.ts
@@ -4,7 +4,7 @@ import ParentUnit from "./plugins/parentunit";
 import {IconToggleDropDown, Icon, IconToggleMenu, IconSortUp, IconSortDown} from "./components/IconComponent";
 import {AutoComponent, AutoComponents} from "./components/AutoComponent";
 import {GridComponent} from "./components/gridcomponent";
-import {FormContainer} from "./components/form";
+import {FormContainer, Uploader} from "./components/form";
 import {join} from '../../../../kernel/src/common'
 import {init} from '../../../../kernel/src/web/init';
 
@@ -23,5 +23,6 @@ Vue.component('w.auto', AutoComponent);
 Vue.component('w.autos', AutoComponents);
 Vue.component('w.grid', GridComponent);
 Vue.component('w.form', FormContainer);
+Vue.component('w.upload', Uploader);
 
 init();
