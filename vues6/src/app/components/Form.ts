971686b1776ec1afc5ede7bc2241d62743ef1267
mark.yuan <mark.yuan@greendotcorp.com>
2017-05-16 21:54:32 +0800

    Simple preview

--- vues6/src/app/components/Form.ts
+++ vues6/src/app/components/Form.ts
@@ -1,7 +1,7 @@
 import * as Vue from 'vue';
 import Component from 'vue-class-component';
 import {Widget} from './widget';
-import {extend, find, clone, all, add, uid} from '../../../../../kernel/src/common';
+import {extend, find, clone, all, add, uid, clear} from '../../../../../kernel/src/common';
 import {send} from '../../../../../kernel/src/web/network';
 
 @Component({
@@ -34,36 +34,86 @@ export class FormContainer extends Widget{
     }
 }
 
-function readURL(el:any, input:any) {
+function readURL(imgel:any, file:any) {
 
-    if (input.files && input.files[0]) {
+    if (file) {
         var reader = new FileReader();
         reader.onload = function (e:any) {
-            el.setAttribute('src', e.target.result);
+            imgel.setAttribute('src', e.target.result);
         }
-        reader.readAsDataURL(input.files[0]);
+        reader.readAsDataURL(file);
     }
 }
+
+
+@Component({
+    template: `
+        <div class="w-wrap w-boundary" :style="show()">
+            <w.icon-placeholder v-if="getfile() && !isimg()" classes="preview" vwidth=500 vheight=500 />
+            <img :style="showimg()" class="preview" ref="img" />
+        </div>
+    `
+    , props:["files"]
+})
+export class SimplePreview extends Widget{
+    _file:any;
+    getfile(){
+        return this._file;
+    }
+    show(){
+        return this.getfile()?'':'display:none';
+    }
+    showimg(){
+        if (this.isimg()){
+            return '';
+        }
+        return 'display:none';
+    }
+    isimg(){
+        console.log(this._file ? this._file.type : '###');
+        return this._file && this._file.type.indexOf('image/') == 0;
+    }
+    update(file:any){
+        this._file = file;
+        if (file){
+            if (this.isimg()){
+                readURL(this.$refs.img, file);
+            }
+        }
+        this.$forceUpdate();
+    }
+}
+
 
 @Component({
     template: `
         <div class="w-boundary w-upload">
             <w.form ref="frm" :action="action" type="upload">
-                <w.icon-placeholder classes="preview" vwidth=500 vheight=500 />
-                <img class="preview" ref="img" />
                 <input ref="box" :id="gid()" name="files" type="file" @change="filechanged()" />
                 <label ref="label" style="cursor:pointer;">
-                    <div ref="text">Drag Here - Click Here - Paste Here to upload</div>
+                    <div class="content" ref="text">
+                        <SimplePreview ref="preview" :style="showpreview()" />
+                        <span>Drag Here - Click Here - Paste Here to upload</span>
+                    </div>
                 </label>
             </w.form>
         </div>
     `
     , props:["action", "auto"]
+    , components:{
+        SimplePreview
+    }
 })
 export class Uploader extends Widget{
     action:string;
     auto:boolean;
-
+    protected changed:boolean;
+    showpreview(){
+        if (this.changed){
+            return '';
+        }
+        return 'display:none';
+    }
     submitting(event:Event){
         event.preventDefault();
     }
@@ -76,6 +126,8 @@ export class Uploader extends Widget{
     filechanged(){
         let form = <HTMLFormElement>this.$el;
         let fd = new FormData(form);
+        this.changed = true;
+        this.$forceUpdate();
         if (this.auto){
             this.output('Uploading ...');
             send(this.action, {form:fd}, 'post').then((o)=>{
@@ -85,9 +137,10 @@ export class Uploader extends Widget{
             });
         }else{
             //let path = (<any>this.$refs.box).value;
-            let img = this.$refs.img;
-            let inp = this.$refs.box;
-            readURL(img, inp);
+            let p = <SimplePreview>this.$refs.preview;
+            let inp = <any>this.$refs.box;
+            p.update(inp.files[0]);
+            //readURL(img, inp);
             //this.output(path);
         }
     }
@@ -100,6 +153,11 @@ export class Uploader extends Widget{
         frm.onsubmit = function(event:Event){
             event.preventDefault();
         }
+        let box = <any>this.$refs.box;
+        let label = <any>this.$refs.label;
+        label.setAttribute('for', box.id);
+    }
+    updated(){
         let box = <any>this.$refs.box;
         let label = <any>this.$refs.label;
         label.setAttribute('for', box.id);
